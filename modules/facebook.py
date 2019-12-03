5efdc422f83087576a5011845c5b2b0e4ae9fd96
Diego Gobbi Slomp <diegogslomp@gmail.com>
2019-12-03 16:19:45 -0300

    add argparse

--- modules/facebook.py
+++ modules/facebook.py
@@ -1,12 +1,32 @@
 import os
-from .robot import Robot
+import argparse
+from robot import Robot
 
 
 class Facebook(Robot):
     def log_in(self):
         self.driver.get('https://facebook.com')
-        self.driver.find_element_by_id('email').send_keys(
+        self.driver.find_element_by_name('email').send_keys(
             os.environ['FACEBOOK_EMAIL'])
-        self.driver.find_element_by_id('pass').send_keys(
+        self.driver.find_element_by_name('pass').send_keys(
             os.environ['FACEBOOK_PASS'])
         self.driver.find_element_by_id('loginbutton').click()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('function', help='login')
+    parser.add_argument('--email', '--user', '-u', help='facebook email')
+    parser.add_argument('--password', '-p', help='facebook pass')
+    args = parser.parse_args()
+
+    if args.email:
+        os.environ['FACEBOOK_EMAIL'] = args.email
+    if args.password:
+        os.environ['FACEBOOK_PASS'] = args.password
+
+    if args.function == 'login':
+        robot = Facebook()
+        robot.log_in()
+    else:
+        print('Nothing to do')
